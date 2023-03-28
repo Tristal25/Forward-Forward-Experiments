517d3cbd16b728484482339b731869c239202072
siriuscdk <41090414+siriuscdk@users.noreply.github.com>
2023-03-28 01:11:16 -0700

    refactor the repo to get ready for cifar10

--- main.py
+++ main.py
@@ -3,9 +3,54 @@ import torch
 import torch.nn as nn
 from tqdm import tqdm
 from torch.optim import Adam
+import torchvision
 from torchvision.datasets import MNIST
 from torchvision.transforms import Compose, ToTensor, Normalize, Lambda
 from torch.utils.data import DataLoader
+import util.model_components as ff
+import argparse
+import random
+import numpy as np
+
+
+DATASETS = {
+    'mnist': {
+        'num_classes': 10,
+        'img_size': 28,
+        'mean': (0.1307,),
+        'std': (0.3081,),
+    },
+
+    'cifar10': {
+        'num_classes': 10,
+        'img_size': 32,
+        'mean': [0.4914, 0.4822, 0.4465],
+        'std': [0.2470, 0.2435, 0.2616],
+    },
+    'cifar100': {
+        'num_classes': 100,
+        'img_size': 32,
+        'mean': [0.5071, 0.4867, 0.4408],
+        'std': [0.2675, 0.2565, 0.2761],
+    }
+}
+
+def init_parser(parser):
+
+    parser.add_argument('--device', type=str, default='gpu')
+
+    parser.add_argument('--dataset',
+                        type=str.lower,
+                        choices=['mnist','cifar10', 'cifar100'],
+                        default='mnist')
+
+    parser.add_argument('--threshold', default=2.0, type=float)
+    parser.add_argument('--lr', default=0.03, type=float)
+    parser.add_argument('--epochs', default=1000, type=int)
+
+    parser.add_argument('--train_batch_size', default=60000, type=int)
+    parser.add_argument('--test_batch_size', default=10000, type=int)
+    return parser
 
 
 def MNIST_loaders(train_batch_size=50000, test_batch_size=10000):
@@ -30,72 +75,10 @@ def MNIST_loaders(train_batch_size=50000, test_batch_size=10000):
     return train_loader, test_loader
 
 
-def overlay_y_on_x(x, y):
-    """Replace the first 10 pixels of data [x] with one-hot-encoded label [y]
-    """
-    x_ = x.clone()
-    x_[:, :10] *= 0.0
-    x_[range(x.shape[0]), y] = x.max()
-    return x_
 
 
-class Net(torch.nn.Module):
-
-    def __init__(self, dims):
-        super().__init__()
-        self.layers = []
-        for d in range(len(dims) - 1):
-            self.layers += [Layer(dims[d], dims[d + 1]).cuda()]
-
-    def predict(self, x):
-        goodness_per_label = []
-        for label in range(10):
-            h = overlay_y_on_x(x, label)
-            goodness = []
-            for layer in self.layers:
-                h = layer(h)
-                goodness += [h.pow(2).mean(1)]
-            goodness_per_label += [sum(goodness).unsqueeze(1)]
-        goodness_per_label = torch.cat(goodness_per_label, 1)
-        return goodness_per_label.argmax(1)
-
-    def train(self, x_pos, x_neg):
-        h_pos, h_neg = x_pos, x_neg
-        for i, layer in enumerate(self.layers):
-            print('training layer', i, '...')
-            h_pos, h_neg = layer.train(h_pos, h_neg)
 
 
-class Layer(nn.Linear):
-    def __init__(self, in_features, out_features,
-                 bias=True, device=None, dtype=None):
-        super().__init__(in_features, out_features, bias, device, dtype)
-        self.relu = torch.nn.ReLU()
-        self.opt = Adam(self.parameters(), lr=0.03)
-        self.threshold = 2.0
-        self.num_epochs = 1000
-
-    def forward(self, x):
-        x_direction = x / (x.norm(2, 1, keepdim=True) + 1e-4)
-        return self.relu(
-            torch.mm(x_direction, self.weight.T) +
-            self.bias.unsqueeze(0))
-
-    def train(self, x_pos, x_neg):
-        for i in tqdm(range(self.num_epochs)):
-            g_pos = self.forward(x_pos).pow(2).mean(1)
-            g_neg = self.forward(x_neg).pow(2).mean(1)
-            # The following loss pushes pos (neg) samples to
-            # values larger (smaller) than the self.threshold.
-            loss = torch.log(1 + torch.exp(torch.cat([
-                -g_pos + self.threshold,
-                g_neg - self.threshold]))).mean()
-            self.opt.zero_grad()
-            # this backward just compute the derivative and hence
-            # is not considered backpropagation.
-            loss.backward()
-            self.opt.step()
-        return self.forward(x_pos).detach(), self.forward(x_neg).detach()
 
     
 def visualize_sample(data, name='', idx=0):
@@ -104,27 +87,90 @@ def visualize_sample(data, name='', idx=0):
     plt.title(name)
     plt.imshow(reshaped, cmap="gray")
     plt.show()
-    
+
+def set_seed(seed_value=42):
+    """Set seed for reproducibility.
+    """
+    random.seed(seed_value)
+    np.random.seed(seed_value)
+    torch.manual_seed(seed_value)
+    torch.cuda.manual_seed_all(seed_value)
+
+def train_top_module(args):
+    set_seed(42)
+
+    if args.device=='gpu' and torch.cuda.is_available():
+        device = torch.device("cuda")
+        print(f'There are {torch.cuda.device_count()} GPU(s) available.')
+        print('Device name:', torch.cuda.get_device_name(0))
+
+    else:
+        print('No GPU available, using the CPU instead.')
+        device = torch.device("cpu")
+
+    print (f'config: {args}')
+
+    num_classes = DATASETS[args.dataset]['num_classes']
+    img_mean, img_std = DATASETS[args.dataset]['mean'], DATASETS[args.dataset]['std']
+    transform = Compose([
+        ToTensor(),
+        Normalize(img_mean, img_std),
+        Lambda(lambda x: torch.flatten(x))])
+
+
+    if args.dataset=='mnist':
+        trainset = torchvision.datasets.MNIST(
+            root='./data', train=True, download=True, transform=transform)
+        testset = torchvision.datasets.MNIST(
+            root='./data', train=False, download=True, transform=transform)
+
+    print (f"Size of trainset: {len(trainset)}")
+    print (f"Size of testset: {len(testset)}")
+
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=args.train_batch_size, shuffle=True, num_workers=0)
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=args.test_batch_size, shuffle=False, num_workers=0)
+
+    print (f"Size of trainloader: {len(trainloader)}")
+    print (f"Size of testloader: {len(testloader)}")
+
+
+
+    #train_loader, test_loader = MNIST_loaders()
+
+    net = ff.Net([784, 500, 500], device, args)  # 2 linear layers with 784=>500, 500=>500
+
+    # start training
+    for batch_idx, (images, target) in enumerate(trainloader):
+        images, target = images.to(device), target.to(device)
+
+        x_pos = ff.overlay_y_on_x(images, target)
+
+        rnd = torch.randperm(images.size(0))
+        x_neg = ff.overlay_y_on_x(images, target[rnd])
+
+        net.train(x_pos, x_neg)
+
+        print('train error:', 1.0 - net.predict(images).eq(target).float().mean().item())
+        print('train acc:', net.predict(images).eq(target).float().mean().item())
+
+
+    # start evaluation
+    with torch.no_grad():
+        for batch_idx, (images, target) in enumerate(testloader):
+            images, target = images.to(device), target.to(device)
+
+
+            print('test error:', 1.0 - net.predict(images).eq(target).float().mean().item())
+            print('test acc:', net.predict(images).eq(target).float().mean().item())
+
+
     
 if __name__ == "__main__":
-    torch.manual_seed(1234)
-    train_loader, test_loader = MNIST_loaders()
+    parser = argparse.ArgumentParser(description='FF training script')
+    parser = init_parser(parser)
+    args = parser.parse_args()
 
-    net = Net([784, 500, 500])
-    x, y = next(iter(train_loader))
-    x, y = x.cuda(), y.cuda()
-    x_pos = overlay_y_on_x(x, y)
-    rnd = torch.randperm(x.size(0))
-    x_neg = overlay_y_on_x(x, y[rnd])
-    
-    for data, name in zip([x, x_pos, x_neg], ['orig', 'pos', 'neg']):
-        visualize_sample(data, name)
-    
-    net.train(x_pos, x_neg)
+    train_top_module(args)
 
-    print('train error:', 1.0 - net.predict(x).eq(y).float().mean().item())
-
-    x_te, y_te = next(iter(test_loader))
-    x_te, y_te = x_te.cuda(), y_te.cuda()
-
-    print('test error:', 1.0 - net.predict(x_te).eq(y_te).float().mean().item())
---
+++ util/model_components.py
@@ -0,0 +1,82 @@
+import torch
+import torch.nn as nn
+from torch.optim import Adam
+from tqdm import tqdm
+
+def overlay_y_on_x(x, y):
+    """Replace the first 10 pixels of data [x] with one-hot-encoded label [y]
+    """
+    x_ = x.clone()
+    x_[:, :10] *= 0.0
+    x_[range(x.shape[0]), y] = x.max()
+    return x_
+
+
+class Net(nn.Module):
+
+    def __init__(self, dims, device, args):
+        super().__init__()
+        self.layers = []
+        for d in range(len(dims) - 1):
+            self.layers += [Layer(dims[d], dims[d + 1], args=args).to(device)]
+
+    def predict(self, x):
+        goodness_per_label = []
+        for label in range(10):
+            h = overlay_y_on_x(x, label)
+            goodness = []
+            for layer in self.layers:
+                h = layer(h)
+                goodness += [h.pow(2).mean(1)]
+
+            #print (len(goodness), goodness)
+            goodness_per_label += [sum(goodness).unsqueeze(1)]
+        goodness_per_label = torch.cat(goodness_per_label, 1)
+        return goodness_per_label.argmax(1)
+
+    def train(self, x_pos, x_neg):
+        h_pos, h_neg = x_pos, x_neg
+        for i, layer in enumerate(self.layers):
+            print(f'training layer {i} ...')
+            h_pos, h_neg = layer.train(h_pos, h_neg)
+
+
+class Layer(nn.Linear):
+    def __init__(self, in_features, out_features,
+                 bias=True, device=None, dtype=None, args=None):
+        super().__init__(in_features, out_features, bias, device, dtype)
+        self.relu = torch.nn.ReLU()
+        self.opt = Adam(self.parameters(), lr=args.lr)
+        self.threshold = args.threshold
+        self.num_epochs = args.epochs
+
+    def forward(self, x):
+        x_direction = x / (x.norm(2, 1, keepdim=True) + 1e-4)
+        return self.relu(
+            torch.mm(x_direction, self.weight.T) +
+            self.bias.unsqueeze(0))
+
+    def train(self, x_pos, x_neg):
+        for i in tqdm(range(self.num_epochs)):
+            #g_pos = self.forward(x_pos).pow(2).mean(1)  # take mean on the axis=1
+            # output is [50000, 500] => [50000, 1]
+            g_pos = torch.mean(torch.pow(self.forward(x_pos), 2), 1)
+            #print (f"g_pos:{g_pos.shape}")
+
+            #g_neg = self.forward(x_neg).pow(2).mean(1)
+            # output is [50000, 500] => [50000, 1]
+            g_neg = torch.mean(torch.pow(self.forward(x_neg), 2), 1)
+
+            # The following loss pushes pos (neg) samples to
+            # values larger (smaller) than the self.threshold.
+
+
+            loss = torch.log(1 + torch.exp(torch.cat([
+                -(g_pos - self.threshold),
+                g_neg - self.threshold]))).mean()
+            self.opt.zero_grad()
+            # this backward just compute the local derivative on current layer
+            # and hence is not considered backpropagation.
+            loss.backward()
+            self.opt.step()
+        return self.forward(x_pos).detach(), self.forward(x_neg).detach()
