6c55cbd53df0b8c3d549a2b5ba1f7e3a24d237b0
siriuscdk <41090414+siriuscdk@users.noreply.github.com>
2023-03-29 23:18:46 -0700

    further refactor and added cifar10

--- main.py
+++ main.py
@@ -17,19 +17,23 @@ DATASETS = {
     'mnist': {
         'num_classes': 10,
         'img_size': 28,
+        'num_channel': 1,
         'mean': (0.1307,),
         'std': (0.3081,),
+
     },
 
     'cifar10': {
         'num_classes': 10,
         'img_size': 32,
+        'num_channel': 3,
         'mean': [0.4914, 0.4822, 0.4465],
         'std': [0.2470, 0.2435, 0.2616],
     },
     'cifar100': {
         'num_classes': 100,
         'img_size': 32,
+        'num_channel': 3,
         'mean': [0.5071, 0.4867, 0.4408],
         'std': [0.2675, 0.2565, 0.2761],
     }
@@ -44,12 +48,14 @@ def init_parser(parser):
                         choices=['mnist','cifar10', 'cifar100'],
                         default='mnist')
 
-    parser.add_argument('--threshold', default=2.0, type=float)
-    parser.add_argument('--lr', default=0.03, type=float)
-    parser.add_argument('--epochs', default=1000, type=int)
+    parser.add_argument('--threshold', default=0.5, type=float)
+    parser.add_argument('--lr', default=0.06, type=float)
+    parser.add_argument('--epochs', default=100, type=int)
 
-    parser.add_argument('--train_batch_size', default=60000, type=int)
+    parser.add_argument('--train_batch_size', default=10000, type=int)
     parser.add_argument('--test_batch_size', default=10000, type=int)
+
+    parser.add_argument('--print_freq', default=20, type=int)
     return parser
 
 
@@ -120,9 +126,15 @@ def train_top_module(args):
 
 
     if args.dataset=='mnist':
+
         trainset = torchvision.datasets.MNIST(
             root='./data', train=True, download=True, transform=transform)
         testset = torchvision.datasets.MNIST(
+            root='./data', train=False, download=True, transform=transform)
+    elif args.dataset == 'cifar10':
+        trainset = torchvision.datasets.CIFAR10(
+            root='./data', train=True, download=True, transform=transform)
+        testset = torchvision.datasets.CIFAR10(
             root='./data', train=False, download=True, transform=transform)
 
     print (f"Size of trainset: {len(trainset)}")
@@ -140,31 +152,85 @@ def train_top_module(args):
 
     #train_loader, test_loader = MNIST_loaders()
 
-    net = ff.Net([img_size*img_size, 500, 500], device, args)  # 2 linear layers with 784=>500, 500=>500
+    net = ff.Net([DATASETS[args.dataset]['num_channel']*img_size*img_size, 100, 100], device, args)  # 2 linear layers with 784=>500, 500=>500
 
     # start training
-    for batch_idx, (images, target) in enumerate(trainloader):
+
+
+        #net.train(x_pos, x_neg)
+
+
+
+    tot_num_batch=len(trainloader)
+
+    for train_batch_idx, (images, target) in enumerate(trainloader):
         images, target = images.to(device), target.to(device)
 
-        x_pos = ff.overlay_y_on_x(images, target)
+        print (f"train_batch: [{train_batch_idx}|{tot_num_batch-1}]")
 
-        rnd = torch.randperm(images.size(0))
-        x_neg = ff.overlay_y_on_x(images, target[rnd])
-
-        net.train(x_pos, x_neg)
-
-        print('train error:', 1.0 - net.predict(images).eq(target).float().mean().item())
-        print('train acc:', net.predict(images).eq(target).float().mean().item())
+        for layeridx, curlayer in enumerate(net.layers):
 
 
-    # start evaluation
-    with torch.no_grad():
-        for batch_idx, (images, target) in enumerate(testloader):
-            images, target = images.to(device), target.to(device)
+            if layeridx == 0:  # only first layer get data, otherwise get previous forward data
+                #print (f"images.shape: {images.shape}")
+
+                x_pos = ff.overlay_y_on_x(images, target)
+
+                rnd = torch.randperm(images.size(0))
+                x_neg = ff.overlay_y_on_x(images, target[rnd])
+
+            for epochidx in range(args.epochs):
+                if epochidx%args.print_freq==0:
+                    print (f"train_batch: [{train_batch_idx}|{tot_num_batch-1}] Layer: [{layeridx:}] Epoch: [{epochidx}]")
 
 
-            print('test error:', 1.0 - net.predict(images).eq(target).float().mean().item())
-            print('test acc:', net.predict(images).eq(target).float().mean().item())
+                #g_pos = self.forward(x_pos).pow(2).mean(1)  # take mean on the axis=1
+                # output is [50000, 500] => [50000, 1]
+                g_pos = torch.mean(torch.pow(curlayer.forward(x_pos), 2), 1)
+                #print (f"g_pos:{g_pos.shape}")
+
+                #g_neg = self.forward(x_neg).pow(2).mean(1)
+                # output is [50000, 500] => [50000, 1]
+                g_neg = torch.mean(torch.pow(curlayer.forward(x_neg), 2), 1)
+
+                # The following loss pushes pos (neg) samples to
+                # values larger (smaller) than the self.threshold.
+
+                # exp is to ensure positive
+                loss = torch.log(1 + torch.exp(torch.cat([
+                    -(g_pos - args.threshold),
+                    g_neg - args.threshold]))).mean()
+
+                curlayer.opt.zero_grad()
+                # this backward just compute the local derivative on current layer
+                # and hence is not considered backpropagation.
+                loss.backward()
+                curlayer.opt.step()
+
+            # finished training on curlayer, pass to next layer
+            x_pos, x_neg=curlayer.forward(x_pos).detach(), curlayer.forward(x_neg).detach()
+
+
+        train_acc=net.predict(images).eq(target).float().mean().item()
+
+
+
+        # start evaluation
+        with torch.no_grad():
+            acc1_num_sum = 0
+            num_input_sum=0
+            for val_batch_idx, (images, target) in enumerate(testloader):
+                images, target = images.to(device), target.to(device)
+
+                cur_val_acc=net.predict(images).eq(target).float().mean().item()
+
+                acc1_num_sum += float(cur_val_acc * images.shape[0])
+                num_input_sum+=images.shape[0]
+
+
+
+            print('train acc:',train_acc)
+            print('test acc:', acc1_num_sum/num_input_sum)
 
 
     
